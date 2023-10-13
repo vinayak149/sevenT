72d5b70c5e376804f71cf2bc016c6c6010951937
vinayak149 <vinayak.sharma7149@gmail.com>
2023-10-14 01:53:26 +0530

    changes

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,67 +1,70 @@
-const passport = require('passport');
+const passport = require("passport");
 
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 
-const User = require('../models/users');
+const User = require("../models/user");
 
-//authentication using passport
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-},
-    //find user and establish identity
-    function (email, password, done) {
-        User.findOne({ email: email }, function (err, user) {
-            if (err) {
-                
-                console.log('Error in finding user');
-                return done(err);
-            }
-
-            if (!user || user.password != password) {
-                console.log('Invalid username/password');
-                return done(null, false);
-            }
-
-            return done(null, user);
-        });
-    }
-));
-
-// serializing user to decide which key is to be kept in cookies
-passport.serializeUser(function (user, done) {
-    done(null, user.id); 
-});
-
-// deserializing user from cookies
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
+// authentication using passport
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passReqToCallback: true,
+    },
+    function (req, email, password, done) {
+      // find a user and establish the identity
+      User.findOne({ email: email }, function (err, user) {
         if (err) {
+          req.flash("error", err);
+          return done(err);
+        }
 
-            console.log('Error in finding user');
-            return done(err);
+        if (!user || user.password != password) {
+          req.flash("error", "Invalid Username/Password");
+          return done(null, false);
         }
 
         return done(null, user);
-    });
+      });
+    }
+  )
+);
+
+// serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
-//check if the user is authenticated
-passport.checkAuthentication = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }  
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log("Error in finding user --> Passport");
+      return done(err);
+    }
 
-    //if user is not signed in
-    return res.redirect('/users/sign-in');
+    return done(null, user);
+  });
+});
+
+// check if the user is authenticated
+passport.checkAuthentication = function (req, res, next) {
+  // if the user is signed in, then pass on the request to the next function(controller's action)
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // if the user is not signed in
+  return res.redirect("/users/sign-in");
 };
 
 passport.setAuthenticatedUser = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        //req.user contains current signed in user from session cookie
-        res.locals.user = req.user;
-    }
-    next();
-}
+  if (req.isAuthenticated()) {
+    // req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
+    res.locals.user = req.user;
+  }
 
+  next();
+};
 
 module.exports = passport;
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,26 +1,37 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
+const User = require("../models/user");
 
+module.exports.home = async function (req, res) {
+  try {
+    // populate the user of each post
+    let posts = await Post.find({})
+      .sort("-createdAt")
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
+      });
 
-module.exports.home = function (req, res) {
-   
-    // res.cookie('user_id', 25);
-    
-    // Post.find({}, function (err, posts) {
-    //     return res.render('home', {
-    //         title: "sevenT | Home",
-    //         posts: posts
-    //     });
-    // });
+    let users = await User.find({});
 
-
-    //populate the user of each post
-    Post.find({}).populate('user').exec(function (err, posts) {
-        return res.render('home', {
-            title: "sevenT | Home",
-            posts: posts
-        });
-    })
-    
-}
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+      all_users: users,
+    });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
+};
 
 // module.exports.actionName = function(req, res){}
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
